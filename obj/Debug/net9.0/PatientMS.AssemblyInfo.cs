4a40ec86dccab51be5b97078d05a50c9812cbc1d
Anirudh Unnikrishnan <anirudh.sowbhagya@gmail.com>
2026-06-23 12:02:47 +0300

    update project with new features and UI changes

--- obj/Debug/net9.0/PatientMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/PatientMS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0941d9bc85678c78b240ee620fa2b7395a8f52ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe1ccf879228475605968ba42fa78d63cc9c3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
